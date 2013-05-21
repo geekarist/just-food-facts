fc1497c65e2a1a61897b718155936e798ad89e2f
Christophe <christophe.pele@gmail.com>
2013-05-21 20:59:45 +0200

    Create unit test for warehouseman

---
+++ backend/main.spec.js
@@ -0,0 +1,23 @@
+var request = require('request')
+var expect = require('chai').expect
+
+describe('Warehouseman', function() {
+	it('should get food', function(done) {
+		// GIVEN
+		var food = 'mozza'
+
+		// WHEN
+		request.get('http://localhost:4000/get-food-facts?q=' + food, function(error, response, body) {
+
+			// THEN
+			expect(error).to.be.null
+			expect(body).to.be.equal([
+				{name: 'Mozzarella', energy: '300', proteins: '13', carbohydrates: '8', fat: '45'},
+				{name: 'Pizza à la mozzarella', energy: '160', proteins: '8', carbohydrates: '9', fat: '12'},
+				{name: 'Tomate mozzarella', energy: '50', proteins: '4', carbohydrates: '3', fat: '9'},
+				{name: 'Pain au maïs', energy: '100', proteins: '3', carbohydrates: '40', fat: '5'}
+				])
+			done()
+		})
+	})
+})
