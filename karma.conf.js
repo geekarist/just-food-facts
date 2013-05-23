87bf8420eb50e0486e26e0e17c4a95b017eb0788
Christophe <christophe.pele@gmail.com>
2013-05-23 07:49:12 -0400

    Import genesis-skeleton

--- karma.conf.js
+++ karma.conf.js
@@ -1,56 +1,20 @@
-// Karma configuration
-
-// base path, that will be used to resolve files and exclude
 basePath = '';
 
-// list of files / patterns to load in the browser
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'app/components/angular/angular.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
+  'components/angular/angular.js',
+  'components/angular-mocks/angular-mocks.js',
+  'build/app/scripts/**/*.js',
+  'build/app/test/unit/**/*Spec.js'
 ];
 
-// list of files to exclude
-exclude = [];
-
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
-
-// web server port
-port = 8080;
-
-// cli runner port
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Firefox'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+reporters       = [ 'dots' ];       // 'dots', 'progress', 'junit'
+port            = 9876;
+runnerPort      = 9100;
+colors          = true;
+logLevel        = LOG_INFO;         // LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+autoWatch       = false;            // Watch for file changes
+browsers        = [ 'PhantomJS' ];  // Chrome, ChromeCanary, Firefox, Opera, Safari (only Mac), PhantomJS, IE (only Windows)
+captureTimeout  = 120 * 1000;
+singleRun       = true;             // Exit upon completion
