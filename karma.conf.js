6005044040c09ee2ae00a4b03896eb5dc7dc8f04
Christophe <christophe.pele@gmail.com>
2013-05-23 20:40:04 +0200

    Add get-food-facts route to express backend

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,6 @@ runnerPort      = 9100;
 colors          = true;
 logLevel        = LOG_INFO;         // LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
 autoWatch       = false;            // Watch for file changes
-browsers        = [ 'PhantomJS' ];  // Chrome, ChromeCanary, Firefox, Opera, Safari (only Mac), PhantomJS, IE (only Windows)
+browsers        = [ 'Firefox' ];  // Chrome, ChromeCanary, Firefox, Opera, Safari (only Mac), PhantomJS, IE (only Windows)
 captureTimeout  = 120 * 1000;
 singleRun       = true;             // Exit upon completion
--- server/lib/api/index.js
+++ server/lib/api/index.js
@@ -10,3 +10,15 @@ app.get('/api/bower', function(req, res) {
 app.get('/api/package', function(req, res) {
   res.send(fs.readFileSync(__dirname + '/../../../package.json'));
 });
+
+app.get('/api/get-food-facts', function(request, response) {
+        var body = JSON.stringify([
+        {name: 'Mozzarella', energy: '300', proteins: '13', carbohydrates: '8', fat: '45'},
+        {name: 'Pizza à la mozzarella', energy: '160', proteins: '8', carbohydrates: '9', fat: '12'},
+        {name: 'Tomate mozzarella', energy: '50', proteins: '4', carbohydrates: '3', fat: '9'},
+        {name: 'Pain au maïs', energy: '100', proteins: '3', carbohydrates: '40', fat: '5'}
+        ]);
+        response.setHeader('Content-type', 'application/json');
+        response.end(body);
+});
+
